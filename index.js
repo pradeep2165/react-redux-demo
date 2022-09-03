0e62dadf5861267e638074f5d0bdc4f8830dcb58
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-03 22:54:20 +0530

    implemented a middleware using redux-logger

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const redux = require("redux");
+const reduxLogger = require("redux-logger");
+
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -62,14 +66,14 @@ const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 console.log("initial state", store.getState());
 //to only subscribe to store
 // store.subscribe(() => console.log("updated state", store.getState()));
 
 //to subscribe and unsubscribe to store
-const unsubscribe = store.subscribe(() => console.log("updated state", store.getState()));
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
