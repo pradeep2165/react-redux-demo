4fb7bea5ebacf627304fb1502f22496f19a6c26d
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-03 21:07:01 +0530

    reducer created;

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ function buyCake() {
     info: "first redux action",
   };
 }
+
+//reducer
+//(previousState, action) => newState
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
