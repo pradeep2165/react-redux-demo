6e32749b01cebd8ba834a4a6dd87a2b96d5adb90
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-04 08:48:45 +0530

    data fetched asynchronously using axios and thunk middleware

--- asyncAction.js
+++ asyncAction.js
@@ -1,5 +1,9 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+
 const initialState = {
   loading: false,
   users: [],
@@ -16,7 +20,7 @@ const fetchUsersRequest = () => {
   };
 };
 
-const fetchUsersSuccess = () => {
+const fetchUsersSuccess = (users) => {
   return {
     type: FETCH_USERS_SUCCESS,
     payload: users,
@@ -51,4 +55,27 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUsersRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        //response data is the array of users
+        const users = response.data.map((user) => user.id);
+        dispatch(fetchUsersSuccess(users));
+      })
+      .catch((error) => {
+        //error.message
+
+        dispatch(fetchUsersFailure(error.message));
+      });
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(fetchUsers());
